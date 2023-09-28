3d0403eac8162711bc63e3a85cb278249930a40f
AlberthUnamad <alberthariaspuclla@gmail.com>
2023-09-28 17:46:12 -0500

    Primer Commit

---
+++ reservapp/api/routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/",(req, res) => {
+    res.send("Endpoint Auth");
+});
+router.get("/register",(req, res) => {
+    res.send("Endpoint register");
+});
+export default router;
